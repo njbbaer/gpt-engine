395c0c79d0bc9b188ceef852b963d517d5caba8e
Nate Baer <njbbaer@gmail.com>
2022-03-19 23:18:48 -0700

    Combine config and context into a single file

--- gpt3.py
+++ gpt3.py
@@ -1,49 +1,50 @@
 import openai
 import os
-import yaml
+from ruamel.yaml import YAML
 
 from dotenv import load_dotenv
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-
-def read_config(config_filepath):
-    with open(config_filepath, 'r') as file:
-        return yaml.safe_load(file)
+yaml = YAML()
 
 
 def read_context(context_filepath):
-    if os.path.isfile(context_filepath):
-        with open(context_filepath, "r") as file:
-            return file.read()
+    with open(context_filepath, 'r') as file:
+        return yaml.load(file)
 
 
 def write_context(context_filepath, context):
-    with open(context_filepath, "w") as file:
-        file.write(context)
+    with open(context_filepath, 'w') as file:
+        yaml.dump(context, file)
 
 
-def complete(config, context):
-    context += config["start_text"]
+def complete(context):
+    context['prompt'] += context['config'].get('start_text', '')
     new_text = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=context,
-        temperature=config["temperature"],
-        max_tokens=config["max_tokens"],
-        stop=config["stop"],
+        engine='text-davinci-002',
+        prompt=context['prompt'],
+        temperature=context['config']['temperature'],
+        max_tokens=context['config']['max_tokens'],
+        stop=context['config'].get('stop'),
     ).choices[0].text
-    print(f'Generated {int(len(new_text) / 4)} tokens')
-    context += new_text + config["restart_text"]
-
+    print(new_text.strip())
+    context['prompt'] += new_text + context['config'].get('restart_text', '')
     return context
 
 
-if __name__ == "__main__":
-    config = read_config('chat.yml')
-    context = read_context('context.txt')
-    if context:
-        context = complete(config, context)
-    else:
-        context = config['prompt']
-    write_context('context.txt', context)
+if __name__ == '__main__':
+    if not os.path.isfile('context.yml'):
+        context = read_context('prompts/summarize.yml')
+        write_context('context.yml', context)
+
+    while True:
+        text_input = input('> ')
+        context = read_context('context.yml')
+        context['prompt'] += text_input
+        context = complete(context)
+        write_context('context.yml', context)
+
+        if not context['config'].get('continuous', False):
+            break
