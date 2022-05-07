15a4a34363f54cd8e2a2aafbf1e3a4322a7efa4b
Nate Baer <njbbaer@gmail.com>
2022-05-07 01:03:20 -0700

    Make input and output prefix fields functional

--- gpt_agent/static/script.js
+++ gpt_agent/static/script.js
@@ -1,16 +1,14 @@
 // Submit an API request
 $('#submitButton').click(() => {
-  const request_body = JSON.stringify({
-    'engine': 'text-davinci-002',
-    'prompt': $('#promptField').text(),
-  });
-  console.log('Request:', request_body);
+  const request_body = getRequestBody();
+  appendPrompt(getFieldValue('input_prefix'));
+  appendPrompt($('#inputField').val());
   fetch('/api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: request_body,
+    body: JSON.stringify(request_body),
   })
   .then(response => {
     if (response.ok) {
@@ -20,7 +18,8 @@ $('#submitButton').click(() => {
     }
   })
   .then(data => {
-    $('#promptField').text($('#promptField').text() + data.choices[0].text);
+    appendPrompt(getFieldValue('output_prefix'));
+    appendPrompt(data.choices[0].text);
   })
   .catch(response => {
     response.json().then((json) => {
@@ -51,4 +50,26 @@ const flashError = (message) => {
   alert.slideDown(() => {
     setTimeout(() => alert.slideUp(), 5000);
   });
+}
+
+const getRequestBody = () => {
+  const request_body = {
+    'engine': 'text-davinci-002',
+    'prompt': $('#promptField').text(),
+  };
+  request_body['temperature'] = parseFloat(getFieldValue('temperature'))
+  request_body['prompt'] += getFieldValue('input_prefix') + $('#inputField').val() + getFieldValue('output_prefix');
+  return request_body
+}
+
+const appendPrompt = (text) => {
+  $('#promptField').text($('#promptField').text() + text);
+}
+
+const getFieldValue = (key) => {
+  if ($(`.toggleField[name=${key}]`).hasClass('active')) {
+    return $(`.paramField[name=${key}]`).find('input').val().replace('\\n', '\n');
+  } else {
+    return '';
+  }
 }
