98cdcf44ebe16bfba15150c6fe39c9f31486f033
Nate Baer <njbbaer@gmail.com>
2022-05-08 22:08:52 -0700

    Put header in navbar

--- gpt_agent/constants.py
+++ gpt_agent/constants.py
@@ -30,6 +30,7 @@ TEMPLATES = {
     'prompt': 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\\n\\nHuman: Hello, who are you?\\nAI: I am an AI created by OpenAI. How can I help you today?',
     'input_prefix': '\\\\nHuman: ',
     'output_prefix': '\\\\nAI:',
-    'stop': 'Human:'
+    'stop': 'Human:',
+    'max_tokens': 128,
   },
 }
