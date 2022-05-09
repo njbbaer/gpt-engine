0d4bebf1d1fae0a43d5b69de65242ce582c0c168
Nate Baer <njbbaer@gmail.com>
2022-05-08 23:18:26 -0700

    Add Stack Overflow template

--- gpt_agent/constants.py
+++ gpt_agent/constants.py
@@ -28,9 +28,17 @@ INPUT_FIELDS = [
 TEMPLATES = {
   'Chat': {
     'prompt': 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\\n\\nHuman: Hello, who are you?\\nAI: I am an AI created by OpenAI. How can I help you today?',
+    'max_tokens': 256,
     'input_prefix': '\\\\nHuman: ',
     'output_prefix': '\\\\nAI:',
     'stop': 'Human:',
-    'max_tokens': 128,
+  },
+  'Stack Overflow': {
+    'prompt': 'Stack Overflow is a website for professional programmers that features questions and answers on a wide range of topics in computer programming. The following question was answered with a thorough, well-written, and researched response.',
+    'temperature': 0,
+    'max_tokens': 512,
+    'input_prefix': '\\\\n\\\\nQuestion:\\\\n\\\\n',
+    'output_prefix': '\\\\n\\\\nBest Answer:',
+    'stop': 'Question:',
   },
 }
