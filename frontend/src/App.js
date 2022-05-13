99b5973cb6ab87767424be2741ee4cc1c2e8e54d
Nate Baer <njbbaer@gmail.com>
2022-05-12 23:56:55 -0700

    Perform API request on button click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,17 +11,50 @@ class App extends React.Component {
     this.setState({textarea: event.target.value});
   }
 
+  handleGenerate = () => {
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        engine: 'text-davinci-002',
+        prompt: this.state.textarea,
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(data => {
+      const new_textarea = this.state.textarea + data.choices[0].text
+      this.setState({textarea: new_textarea});
+    })
+    .catch(response => {
+      response.json().then((json) => {
+        console.error(json['error']);
+      })
+    });
+  }
+
   render() {
     return (
       <div className="container d-grid gap-3 mt-3">
         <textarea
           className="form-control"
           rows="10"
-          value={this.state.value}
+          value={this.state.textarea}
           onChange={this.handleChangeTextarea}
         />
         <div className="d-flex gap-2 mb-3">
-          <button id="generate-button" className="btn btn-lg btn-primary">Generate</button>
+          <button 
+            id="generate-button"
+            className="btn btn-lg btn-primary"
+            onClick={this.handleGenerate}
+          >Generate</button>
         </div>
       </div>
     );
