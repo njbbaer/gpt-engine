5b84ff4e2d5493a23ceaafa4e0e213ace8536abc
Nate Baer <njbbaer@gmail.com>
2022-05-14 15:29:52 -0700

    Add API key field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,38 @@ import "./App.css";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
 import TextareaAutosize from "react-textarea-autosize";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {textarea: ''};
+    this.state = {
+      textarea: '',
+      apiKey: '',
+    };
   }
 
   handleChangeTextarea = (event) => {
     this.setState({textarea: event.target.value});
   }
 
+  handleChangeApiKey = (event) => {
+    this.setState({apiKey: event.target.value});
+  }
+
+  handleSelectTemplate = (_, event) => {
+    this.setState({selectedTemplate: event.target.text});
+  }
+
   handleGenerate = () => {
-    fetch('/api', {
+    fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.state.apiKey}`,
       },
       body: JSON.stringify({
-        engine: 'text-davinci-002',
         prompt: this.state.textarea,
       })
     })
@@ -44,28 +56,40 @@ class App extends React.Component {
     });
   }
 
-  handleSelectTemplate = (_, event) => {
-    this.setState({selectedTemplate: event.target.text});
-  }
-
   render() {
     return (
       <div className="container d-grid gap-3 mt-3">
-        <Dropdown id="template-dropdown" onSelect={this.handleSelectTemplate}>
-          <Dropdown.Toggle variant="outline-secondary" id="template-dropdown">
-            {this.state.selectedTemplate || "Select a template"}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item>Chat</Dropdown.Item>
-            <Dropdown.Item>Stack Overflow</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <TextareaAutosize
-          className="form-control"
-          style={{ resize: "none" }}
-          value={this.state.textarea}
-          onChange={this.handleChangeTextarea}
-        />
+        <Form.Group>
+          <Form.Label>API Key</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your OpenAI API key"
+            data-lpignore="true"  // Disable LastPass
+            onChange={this.handleChangeApiKey}
+            value={this.state.apiKey}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Template</Form.Label>
+          <Dropdown id="template-dropdown" onSelect={this.handleSelectTemplate}>
+            <Dropdown.Toggle variant="outline-secondary" id="template-dropdown">
+              {this.state.selectedTemplate || "Select a template"}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>Chat</Dropdown.Item>
+              <Dropdown.Item>Stack Overflow</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Prompt</Form.Label>
+          <TextareaAutosize
+            className="form-control"
+            style={{ resize: "none" }}
+            value={this.state.textarea}
+            onChange={this.handleChangeTextarea}
+          />
+        </Form.Group>
         <div className="d-flex gap-2 mb-3">
           <Button 
             id="generate-button"
