98b2bd78571af2ffad4efec3ecac7ffd6f9e4012
Nate Baer <njbbaer@gmail.com>
2022-05-14 01:25:41 -0700

    Create template dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 import React from "react";
-import TextareaAutosize from 'react-textarea-autosize';
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import TextareaAutosize from "react-textarea-autosize";
 
 class App extends React.Component {
   constructor(props) {
@@ -42,22 +44,34 @@ class App extends React.Component {
     });
   }
 
+  handleSelectTemplate = (_, event) => {
+    this.setState({selectedTemplate: event.target.text});
+  }
+
   render() {
     return (
       <div className="container d-grid gap-3 mt-3">
+        <Dropdown id="template-dropdown" onSelect={this.handleSelectTemplate}>
+          <Dropdown.Toggle variant="outline-secondary" id="template-dropdown">
+            {this.state.selectedTemplate || "Select a template"}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>Chat</Dropdown.Item>
+            <Dropdown.Item>Stack Overflow</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <TextareaAutosize
           className="form-control"
-          minRows="8"
-          style={{ resize: 'none' }}
+          style={{ resize: "none" }}
           value={this.state.textarea}
           onChange={this.handleChangeTextarea}
         />
-        <div className="input-bar d-flex gap-2 mb-3">
-          <button 
+        <div className="d-flex gap-2 mb-3">
+          <Button 
             id="generate-button"
             className="btn btn-lg btn-primary"
             onClick={this.handleGenerate}
-          >Generate</button>
+          >Generate</Button>
         </div>
       </div>
     );
