e45a0af8dea3a8cea6d584bd8e9d0810d8ab0f1f
Nate Baer <njbbaer@gmail.com>
2022-05-21 00:34:43 -0700

    Remove extra spaces

--- frontend/src/templates.js
+++ frontend/src/templates.js
@@ -50,25 +50,25 @@ const templates = {
   chat: {
     ...conversationDefaults,
     title: "AI Assistant",
-    prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n Human: Hello, who are you?\n AI: I am an AI created by OpenAI. How can I help you today?",
-    inputPrefix: "\n Human: ",
-    inputSuffix: "\n AI:",
+    prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?",
+    inputPrefix: "\nHuman: ",
+    inputSuffix: "\nAI:",
     stopSequences: "Human:",
   },
   caveJohnsonn: {
     ...conversationDefaults,
     title: "Cave Johnson",
     prompt: "Cave Johnson is the eccentric billionare founder of Aperture Science, a company which has a research policy to \"throw science at the wall and see what sticks\". A cigar-chewing tycoon, Cave is extroverted, enthusiastic, and opinionated. He is a fierce advocate for scientific research with no concern for expense, or the health and safety of his test subjects. Cave is fond of his personal assistant, a woman named Caroline whom he depends on to fulfill the role of executing his directives as well as providing comfort and support for the man in charge, himself. The following is a conversation between Cave Johnson and Caroline:\n",
-    inputPrefix: "\n Caroline: ",
-    inputSuffix: "\n Cave Johnson:",
+    inputPrefix: "\nCaroline: ",
+    inputSuffix: "\nCave Johnson:",
     stopSequences: "Caroline:",
   },
   glados: {
     ...conversationDefaults,
     title: "GLaDOS",
     prompt: "GLaDOS is an artificially superintelligent computer system from the video game series Portal. She is responsible for testing and maintenance in the Aperture Science Computer-Aided Enrichment Center. Her personality is passive-aggressive, witty, narcissistic, and sinister. The following is a conversation between GLaDOS and her test subject Chell:\n",
-    inputPrefix: "\n Chell: ",
-    inputSuffix: "\n GLaDOS:",
+    inputPrefix: "\nChell: ",
+    inputSuffix: "\nGLaDOS:",
     stopSequences: "Chell:",
   }
 };
