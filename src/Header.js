b0fd57af09243e4470f0fa914f280c695fb44609
Nate Baer <njbbaer@gmail.com>
2022-05-24 23:40:12 -0700

    Add icons in header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import Nav from "react-bootstrap/Nav";
+import { ChatLeftText, Github } from "react-bootstrap-icons";
 
 function Header() {
   return (
     <Nav className="navbar navbar-light bg-light">
       <div className="container">
-        <div className="navbar-brand">Language Agent</div>
-        <a href="https://github.com/njbbaer/gpt-agent">GitHub</a>
+        <div className="navbar-brand">
+          <ChatLeftText /> Language Agent
+        </div>
+        <a class="icon-link" href="https://github.com/njbbaer/gpt-agent">
+          <Github size={20} />
+        </a>
       </div>
     </Nav>
   );
